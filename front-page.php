18cfdfb000afaa0b920aa8257b195f425ead74f3
Austin Reilly <areilly@alphaworks.tech>
2021-09-01 09:18:04 -0700

    put together styles and shtuffs for the banner on the home page. Also added front-page.php, hopefully that's the right move. Else reconvene

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<? get_header();?>
+
+
+<main class="homepage">
+	<div class="banner" style="background: url('<? echo get_theme_file_uri('/images/towns-banner.png');?>'); background-size: cover; background-position: top right; background-repeat: no-repeat;">
+			<div class="blue-diagonal" style="background: url('<? echo get_theme_file_uri('/images/blue-diagonal.png');?>')">
+				<div class="banner-text-container">
+					<h1 class="banner__title">
+						American <span class="mustardYellow">Re</span>newal <br> Starts Here
+					</h1>
+					<p class="banner-text">Welcome to the site for the Our Towns Civic Foundation, devoted to connecting and supporting those leading today's American renewal, from the ground up. Please join us to become part of this movement.</p>
+				</div>
+			</div>
+	</div>
+</main>
+
+<? get_footer();?>
