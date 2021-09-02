64698edececd921c98ed996c097f4e94f96d37c9
Austin Reilly <areilly@alphaworks.tech>
2021-09-02 11:55:58 -0700

    I got a little bit of report logic going not sure if it's how they intended or not, but there is now one big first report on the left under reports (which are blogs?) and the rest, up to 4, appear on the right. checkitoout

--- front-page.php
+++ front-page.php
@@ -12,6 +12,63 @@
 				</div>
 			</div>
 	</div>
+
+	<div class="separator">
+		<div class="line-s"></div>
+			<h2>Reports</h2>
+		<div class="line-e"></div>
+	</div>
+
+	<div class="homeReports">
+	<?
+		if (have_posts())
+		{
+			$postNumber = 1;
+			if (is_home() && !is_front_page())
+			{
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?
+			} ?>
+
+			<div class="postOne">
+			<?
+				while (have_posts())
+				{
+					the_post();
+					get_template_part('template-parts/content', 'first-post');
+					echo $postNumber;
+					break;
+				} ?>
+			</div>
+			<div class="genPosts">
+			<?
+				while(have_posts())
+				{
+					if($postNumber === 1)
+					{
+						$postNumber++;
+						continue;
+					}
+					the_post();
+					get_template_part('template-parts/content', get_post_type());
+					echo $postNumber++;
+					if($postNumber>5)break;
+				} ?>
+			</div>
+		<?
+			the_posts_navigation();
+		}
+		else
+		{
+			get_template_part( 'template-parts/content', 'none' );
+		}
+		?>
+	</div>
 </main>
+
+
 
 <? get_footer();?>
