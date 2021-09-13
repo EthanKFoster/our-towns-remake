5a7dff6e701a651145670f8561c877df7ac36433
Austin Reilly <areilly@alphaworks.tech>
2021-09-13 11:06:18 -0700

    kinda almost got a front  page working ish maybe?

--- front-page.php
+++ front-page.php
@@ -18,61 +18,79 @@
 		<div class="line-e"></div>
 	</div>
 
-	<div class="homeReports">
-	<?
-		if(have_posts())
-		{
-			$postNumber = 1;
-			if (is_home() && !is_front_page())
-			{
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<? } ?>
-
-			<div class="postOne">
-			<?
-				while (have_posts())
-				{
-					the_post();
-					get_template_part('template-parts/content', 'first-post');
-					// echo 'post '.$postNumber;
-					break;
-				} ?>
-			</div>
-			<div class="genPosts">
-			<?
-				while(have_posts())
-				{
-					if($postNumber === 1)
-					{
-						$postNumber++;
-						continue;
-					}
-					the_post();
-					get_template_part('template-parts/content', get_post_type());
-					// echo 'post '.$postNumber++;
-					$postNumber++;
-					if($postNumber>5)break;
-				} ?>
-			</div>
+	<div class="container">
+		<div class="row">
 		<?
-			the_posts_navigation();
-		}
-		else
-		{
-			get_template_part( 'template-parts/content', 'none' );
-		} ?>
+			$posts = new WP_Query(array(
+				'post_type' => 'post'
+			));?>
+				<div class="col-6">
+				<?
+					while ($posts->have_posts())
+					{
+						$posts->the_post();
+						get_template_part('template-parts/content', 'first-post');
+						// echo 'post '.$postNumber;
+						break;
+					} ?>
+				</div>
+				<div class="col-6 row justify-content-between">
+				<?
+					while($posts->have_posts())
+					{
+						if($postNumber === 1)
+						{
+							$postNumber++;
+							continue;
+						}
+						$posts->the_post();
+						get_template_part('template-parts/content', get_post_type());
+						// echo 'post '.$postNumber++;
+						$postNumber++;
+						if($postNumber>5)break;
+					} ?>
+				</div>
+		</div>
 	</div>
 
-	<div class="previous-reports">
+	<div class="previous-reports mb-5">
 		<h1>Previous Reports</h1>
 		<p>Read past content from the website here.</p>
 	</div>
 
-	<div class="reports-carousel">
+	<div class="container">
 		<h2 class="reports-carousel-title">Voice From America</h2>
+		<div class="row reports-carousel">
+			<?
+				$postNumber = 1;
+				while($posts->have_posts())
+				{
+					$posts->the_post();
+					?>
+					<div class="col-4">
+						<a href="<? the_permalink();?>">
+							<header class="entry-header">
+							<?php our_towns_post_thumbnail('small');
+								if (is_singular()) :
+									the_title( '<h1 class="entry-title">', '</h1>' );
+								else :
+									?> <h1><? the_title();?></h1><?
+								endif;?>
+							</header><!-- .entry-header -->
+							<div class="entry-content">
+								<? the_excerpt();?>
+							</div> <!-- .entry-content -->
+						</a>
+					</div>
+					<?
+					if($postNumber === 3)
+					{
+						break;
+					}
+					$postNumber++;
+				}
+			?>
+		</div>
 	</div>
 </main>
 
